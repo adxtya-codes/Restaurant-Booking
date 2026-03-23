88d78c7d61f617a94a0b8bc08bc45e816d8d0a6e
Aditya Singh <singh.aditya8213@gmail.com>
2026-03-23 19:18:03 +0530

    Fix puppeteer string binding for Railway deployment

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const userSessions = {};
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: true },
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
+    },
     webVersion: '2.3000.1015901620',
     webVersionCache: {
         type: 'remote',
